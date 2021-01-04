bdc068feb2841b8ab8256f0fb14faaac36ae96ff
Shane Ryan <59435725+SHANERY86@users.noreply.github.com>
2021-01-04 20:24:01 +0000

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var port = process.env.PORT || 8080;
 
 app.set('view engine', 'ejs');
 
-app.use(express.static(path.join(__dirname, 'app/assets')));
+app.use(express.static(__dirname + '/public'));
 
 // set the home page route
 app.get('/', function(req, res) {
