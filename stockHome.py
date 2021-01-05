3483f173384a5cb3d6d14223bba0c8ace25bef5e
Shane Ryan <59435725+SHANERY86@users.noreply.github.com>
2021-01-05 09:54:58 +0000

    Update stockHome.py

--- stockHome.py
+++ stockHome.py
@@ -40,19 +40,23 @@ def writeData(val):
     # Closing the connection
     conn.close()
 
-# Call HX711 class from the hx711.py script, set channel 5 to     
+# Call HX711 class from the hx711.py script, set pin 5 on the GPIO to data input and pin 6 to clock pulse    
 hx = HX711(5, 6)
 
+# This reference unit is for calibration, needs to be calculated when known weight is placed on the scale. You divide the output value by this number to give the correct weight
+# When its done once, its done for all further measurements.
 hx.set_reference_unit(471)
-
 
 hx.reset()
 
+# This is an initial measurement to set a zero point. Whatever is measured during the tare, will not be counted in the final measurement. So this is an 
+# opportunity to place any containers on the scale before adding the items you plan to measure. 
 hx.tare()
 
 input("Tare done! Add weight now and press any key")
 
-
+# assign reading to val, print and send it to the firebase database and thingspeak graphs. Sleep for 15 seconds after each measurement as Thingspeak will not accept data at
+# quicker intervals.
 while True:
     try:
         val = round(hx.get_weight(5),2)
