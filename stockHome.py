971bf9bba558b58a29175073bacf1b97fab3703f
Shane Ryan <59435725+SHANERY86@users.noreply.github.com>
2021-01-05 09:48:47 +0000

    Update stockHome.py

--- stockHome.py
+++ stockHome.py
@@ -9,19 +9,25 @@ from firebase_admin import credentials, firestore, storage, db
 import os
 from hx711 import HX711
 import RPi.GPIO as GPIO
+
+# take argument with command to run the code and append it to "item" variable.
 item = sys.argv[1]
+# ignore inconsequential warnings when starting
 GPIO.setwarnings(False)
 
+# Firebase setup
 cred=credentials.Certificate('./serviceAccountKey.json')
 firebase_admin.initialize_app(cred, {
     'databaseURL': 'https://sensepi-c7a8f.firebaseio.com/'
 })
 weightdb = db.reference(item)
 
+# Thingspeak setup
 WRITE_API_KEY='TE6KKXKBIBDN5I29'
 
 baseURL='https://api.thingspeak.com/update?api_key=%s' % WRITE_API_KEY
 
+# depending on argument entered when starting the script, the data will go into the relevant graph on Thingspeak
 def writeData(val):
     # Sending the data to thingspeak in the query string
     if item == "potatoes":
@@ -34,6 +40,7 @@ def writeData(val):
     # Closing the connection
     conn.close()
 
+# Call HX711 class from the hx711.py script, set channel 5 to     
 hx = HX711(5, 6)
 
 hx.set_reference_unit(471)
